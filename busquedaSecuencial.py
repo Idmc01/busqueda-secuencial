d488a1fbc7ca34493753f012f84f9f08e74e44c5
Idmc01 <60985426+Idmc01@users.noreply.github.com>
2020-10-31 17:42:15 -0600

    Add files via upload

---
+++ busquedaSecuencial.py
@@ -0,0 +1,22 @@
+def busquedaSecuencial():
+    try:
+        if isinstance(lista,list) and isinstance(elemento,int):
+            posicion = 1
+            for i in lista:
+                if i == elemento:
+                    print("El valor está en la posición: ", posicion)
+                    return True
+                else:
+                    posicion+=1
+            return False
+        else:
+            print("Ingrese un valor válido")
+            busquedaSecuencial()
+    except:
+        print("Ingrese un valor válido")
+        busquedaSecuencial()
+        
+lista = eval(input("Ingrese la lista a evaluar: "))
+elemento = int(input("Ingrese el elemento a buscar: "))
+print(busquedaSecuencial())
+
